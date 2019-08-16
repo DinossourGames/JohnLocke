711a9c5481b2c47d524ecb38a874e431193ab3e1
Felipe Oliveira <alef@dinossour.com.br>
2019-08-16 17:15:33 -0300

    Dll ds4 add

--- Assets/Scripts/Sample.cs
+++ Assets/Scripts/Sample.cs
@@ -1,18 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DS4_Wrapper;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    private DS4Manager ds4;
+
+    private void Awake()
+    {
+        ds4 = DS4Manager.Instancia;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        ds4.Initialize(false);
+        ds4.SetLights(new Color32(255, 0, 255, 255));
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
