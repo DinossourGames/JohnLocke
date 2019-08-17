1fbd403091ac04b81f3ed7f12395f1c7e8a2c8d5
Alef <alef@dinossour.com.br>
2019-08-17 20:33:24 -0300

    Boss Fight 001
    folder structure
    boss game manager add
    test images folder

---
+++ Assets/Scripts/BossFights/Patricio/BossFightManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BOSS_FIGHT_STATE { STARTING, STAGE1, STAGE2, STAGE3, FINISHING }
+
+
+public class BossFightManager : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
