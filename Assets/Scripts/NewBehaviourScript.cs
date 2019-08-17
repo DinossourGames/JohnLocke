f975163f01636cb18fc52df44470a56d2809a9a6
Alef <alef@dinossour.com.br>
2019-08-17 18:03:12 -0300

    Loading System

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public Button btn;
+
+    private void Awake()
+    {
+        btn.onClick.AddListener(Log);
+    }
+
+    private void Log()
+    {
+        SceneManager.LoadScene("b", 1);
+    }
+}
---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneManager : MonoBehaviour
+{
+    [SerializeField] Sprite[] BgImages = default;
+    [SerializeField] Image Background = default;
+    [SerializeField] Image LoadingBar = default;
+
+    public static string sceneToLoad;
+    public static int index;
+
+    private void Start()
+    {
+        if (sceneToLoad != null && index <= BgImages.Length)
+        {
+            StartCoroutine(LoadSceneAsync(sceneToLoad, index));
+        }
+    }
+
+    public static void LoadScene(string sceneName, int bgIndex)
+    {
+        sceneToLoad = sceneName;
+        index = bgIndex;
+        UnityEngine.SceneManagement.SceneManager.LoadSceneAsync("LoadingScreen");
+    }
+
+ 
+  
+
+    IEnumerator LoadSceneAsync(string sceneName, int bgIndex)
+    {
+        Background.sprite = BgImages[bgIndex];
+
+        float duration = 5f;
+
+        float totalTime = 0;
+        while (totalTime <= duration)
+        {
+            LoadingBar.fillAmount = totalTime / duration;
+            totalTime += Time.deltaTime;
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(.7f);
+
+        UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(sceneName);
+
+    }
+
+}
