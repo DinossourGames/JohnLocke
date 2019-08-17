c5395475f38598e02c7e6e6d136019c5bfd3db84
Alef <alef@dinossour.com.br>
2019-08-17 20:38:53 -0300

    Update BossFightManager.cs

--- Assets/Scripts/BossFights/Patricio/BossFightManager.cs
+++ Assets/Scripts/BossFights/Patricio/BossFightManager.cs
@@ -7,6 +7,9 @@ public enum BOSS_FIGHT_STATE { STARTING, STAGE1, STAGE2, STAGE3, FINISHING }
 
 public class BossFightManager : MonoBehaviour
 {
+
+    [Header("BOSS MANAGER STATS")]
+    [SerializeField] BOSS_FIGHT_STATE fightState;
     void Start()
     {
         
