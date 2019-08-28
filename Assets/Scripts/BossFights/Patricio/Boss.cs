4c9aa59d867a9b27ae72ff247478f518dabbd2e3
Felipe Alef <alef@dinossour.com.br>
2019-08-28 05:50:14 -0300

    Boss Animations Fixed

--- Assets/Scripts/BossFights/Patricio/Boss.cs
+++ Assets/Scripts/BossFights/Patricio/Boss.cs
@@ -4,22 +4,46 @@ using UnityEngine;
 
 public class Boss : MonoBehaviour
 {
-
     [SerializeField] private Animator bossAnimator;
-    
+    [SerializeField] private Transform shootPoint;
+    [SerializeField] private Transform missilShootPoint;
+    [SerializeField] private GameObject armGun;
+
+    [SerializeField, Header("Prefabs"), Space]
+    private GameObject bulletPrefab;
+
+    [SerializeField] private GameObject missilPrefab;
+    [SerializeField] private GameObject missilParryPrefab;
+
+    [SerializeField] private Patricio _patricio;
+
+    private bool isFacingRight;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _patricio = FindObjectOfType<Patricio>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Flip();
     }
-    
-    
-    
-    
+
+
+    private void Flip()
+    {
+        if (transform.position.x - _patricio.transform.position.x < 0 && !isFacingRight)
+        {
+            isFacingRight = true;
+            var localScale = transform.localScale;
+            transform.localScale = new Vector3(localScale.x * -1, localScale.y, localScale.z);
+        }
+
+        if (!(transform.position.x - _patricio.transform.position.x > 0) || !isFacingRight) return;
+        isFacingRight = false;
+        var scale = transform.localScale;
+        transform.localScale = new Vector3(scale.x * -1, scale.y, scale.z);
+    }
 }
